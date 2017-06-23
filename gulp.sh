c09a667e1e929f8a6381d422c65b62db0f9d4ea5
udit01 <uditjainiitd@gmail.com>
2017-06-23 11:35:55 +0530

    navbar done

--- gulp.sh
+++ gulp.sh
@@ -1,17 +1,22 @@
-sudo npm install gulp
-sudo npm install gulp-util --save
-sudo npm install gulp-minify --save
-sudo npm install gulp-jshint --save
-sudo npm install gulp-rename --save
-sudo npm install gulp-concat --save
-sudo npm install gulp-uglifycss --save
-sudo npm install gulp-clean --save
-sudo npm install gulp-compass --save
-sudo npm install del --save
-sudo npm install run-sequence --save
 
-sudo gem install -n /usr/local/bin compass  --save
-sudo gem install -n /usr/local/bin sass  --save
+
+npm install gulp
+npm install gulp-util --save
+npm install gulp-minify --save
+npm install jshint --save
+npm install jshint-stylish --save
+npm install gulp-jshint --save
+npm install gulp-rename --save
+npm install gulp-concat --save
+npm install gulp-uglifycss --save
+npm install gulp-clean --save
+npm install gulp-compass --save
+npm install gulp-connect --save
+npm install del --save
+npm install run-sequence --save
+
+sudo gem install -n /usr/local/bin compass
+sudo gem install -n /usr/local/bin sass
 bower install angularjs  --save
 bower install angular-material  --save
 bower install angular-route  --save
